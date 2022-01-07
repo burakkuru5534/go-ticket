5555fc89e40ce10fa13536ec9edcc5c513474d7b
Burak Kuru <burak.kuru@abkholding.com>
2022-01-07 18:52:49 +0300

    dockerize process

--- main.go
+++ main.go
@@ -38,18 +38,32 @@ func main() {
 
 	defer timeTrack(time.Now(), "purchase process info")
 	fmt.Println("Starting concurrent calls...")
+
+	psqlconn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+
+	// open database
+	db, err := sql.Open("postgres", psqlconn)
+	if err != nil {
+		panic(err)
+	}
+	// close database
+	defer db.Close()
+
+	// check db
+	err = db.Ping()
+
+	fmt.Println("Connected!")
+
+
+	initDB(db)
 	wg := new(sync.WaitGroup)
 
 		//r.Handle("/ticket/{id}/purchases", purchasesFromTicketOptions(wg))
-	go r.Handle("/ticket/{id}/purchases", purchasesFromTicketOptions(wg))
+	go r.Handle("/ticket_options/{id}/purchases", purchasesFromTicketOptions(wg, db))
 
+		r.Handle("/ticket_options/{id}", GetTicketOption(wg, db))
 
-
-
-
-		r.Handle("/ticket/{id}", GetTicketOption(wg))
-
-		r.Handle("/ticket", CreateTicketOption(wg))
+		r.Handle("/ticket_options", CreateTicketOption(wg, db))
 
 
 	//define options
@@ -67,7 +81,7 @@ func timeTrack(start time.Time, name string) {
 	log.Printf("%s took %s", name, elapsed)
 }
 
-func purchasesFromTicketOptions(wg *sync.WaitGroup) http.Handler {
+func purchasesFromTicketOptions(wg *sync.WaitGroup, db *sql.DB) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
 		wg.Wait()
@@ -91,21 +105,7 @@ func purchasesFromTicketOptions(wg *sync.WaitGroup) http.Handler {
 		ticketopt, err := uuid.FromString(ticketOptionsID)
 		purchases.TicketOptionID = uuid2.UUID(ticketopt)
 
-		psqlconn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
 
-		// open database
-		db, err := sql.Open("postgres", psqlconn)
-		if err != nil {
-			http.Error(w, "db connection error", 404)
-
-		}
-		// close database
-		defer db.Close()
-
-		// check db
-		err = db.Ping()
-
-		fmt.Println("Connected!")
 
 		allocation := getAllocationOfTicketOptions(uuid.UUID(purchases.TicketOptionID), db)
 
@@ -143,7 +143,7 @@ func purchasesFromTicketOptions(wg *sync.WaitGroup) http.Handler {
 
 	})
 }
-func GetTicketOption(wg *sync.WaitGroup) http.Handler {
+func GetTicketOption(wg *sync.WaitGroup, db *sql.DB) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		wg.Wait()
 		wg.Add(1)
@@ -156,24 +156,9 @@ func GetTicketOption(wg *sync.WaitGroup) http.Handler {
 		params := mux.Vars(r)
 		id := createKeyValuePairs(params)
 
-		psqlconn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
-
-		// open database
-		db, err := sql.Open("postgres", psqlconn)
-		if err != nil {
-			http.Error(w, "db connection error", 404)
-			return
-		}
-		// close database
-		defer db.Close()
-
-		// check db
-		err = db.Ping()
-
-		fmt.Println("Connected!")
 
 		sq := fmt.Sprintf("select id::text,name,\"desc\",allocation,created_at,updated_at from  ticket_options where id::text = '%s'", id)
-		err = db.QueryRow(sq).Scan(&ticketOption.ID, &ticketOption.Name, &ticketOption.Desc, &ticketOption.Allocation, &ticketOption.CreatedAt, &ticketOption.UpdatedAt)
+		err := db.QueryRow(sq).Scan(&ticketOption.ID, &ticketOption.Name, &ticketOption.Desc, &ticketOption.Allocation, &ticketOption.CreatedAt, &ticketOption.UpdatedAt)
 		if err != nil {
 			http.Error(w, "select from ticket options error", 404)
 			return
@@ -185,7 +170,7 @@ func GetTicketOption(wg *sync.WaitGroup) http.Handler {
 	})
 
 }
-func CreateTicketOption(wg *sync.WaitGroup) http.Handler {
+func CreateTicketOption(wg *sync.WaitGroup, db *sql.DB) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		wg.Wait()
 		wg.Add(1)
@@ -200,21 +185,6 @@ func CreateTicketOption(wg *sync.WaitGroup) http.Handler {
 			return
 		}
 
-		psqlconn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
-
-		// open database
-		db, err := sql.Open("postgres", psqlconn)
-		if err != nil {
-			http.Error(w, "db connection error", 404)
-			return
-		}
-		// close database
-		defer db.Close()
-
-		// check db
-		err = db.Ping()
-
-		fmt.Println("Connected!")
 
 		sq := fmt.Sprintf("insert into ticket_options (name, \"desc\", allocation, created_at, updated_at) values ('%s', '%s', %d, current_timestamp, current_timestamp)", ticketOption.Name.String, ticketOption.Desc.String, ticketOption.Allocation)
 		_, err = db.Exec(sq)
@@ -273,4 +243,160 @@ func getAllocationOfTicketOptions(ticketOptionsID uuid.UUID, db *sql.DB) int64 {
 	}
 
 	return allocation
+}
+
+func initDB (db *sql.DB){
+
+	sq := `
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 9.6.5
+-- Dumped by pg_dump version 9.6.5
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: -
+--
+
+CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
+
+
+--
+-- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: -
+--
+
+COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
+
+
+--
+-- Name: uuid-ossp; Type: EXTENSION; Schema: -; Owner: -
+--
+
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp" WITH SCHEMA public;
+
+
+--
+-- Name: EXTENSION "uuid-ossp"; Type: COMMENT; Schema: -; Owner: -
+--
+
+COMMENT ON EXTENSION "uuid-ossp" IS 'generate universally unique identifiers (UUIDs)';
+
+
+SET search_path = public, pg_catalog;
+
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
+--
+-- Name: ar_internal_metadata; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE if not exists ar_internal_metadata (
+                                                    key character varying NOT NULL,
+                                                    value character varying,
+                                                    created_at timestamp without time zone NOT NULL,
+                                                    updated_at timestamp without time zone NOT NULL,
+
+                                                    constraint ar_internal_metadata_pkey primary key (key)
+
+);
+
+
+--
+-- Name: purchases; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE if not exists purchases (
+                                         id uuid DEFAULT uuid_generate_v4() NOT NULL,
+                                         quantity integer,
+                                         user_id uuid,
+                                         ticket_option_id uuid,
+                                         created_at timestamp without time zone NOT NULL,
+                                         updated_at timestamp without time zone NOT NULL,
+
+                                         constraint purchases_pkey primary key (id)
+
+
+);
+
+
+--
+-- Name: schema_migrations; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE if not exists schema_migrations (
+    version character varying NOT NULL,
+
+    constraint schema_migrations_pkey primary key (version)
+
+);
+
+
+--
+-- Name: ticket_options; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE if not exists ticket_options (
+                                              id uuid DEFAULT uuid_generate_v4() NOT NULL,
+                                              name character varying,
+                                              "desc" character varying,
+                                              allocation integer,
+                                              created_at timestamp without time zone NOT NULL,
+                                              updated_at timestamp without time zone NOT NULL,
+
+                                              constraint ticket_options_pkey primary key (id)
+
+);
+
+
+--
+-- Name: tickets; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE if not exists tickets (
+                                       id uuid DEFAULT uuid_generate_v4() NOT NULL,
+                                       ticket_option_id uuid,
+                                       purchase_id uuid,
+                                       created_at timestamp without time zone NOT NULL,
+                                       updated_at timestamp without time zone NOT NULL,
+                                       constraint tickets_pkey primary key (id)
+
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+--
+-- PostgreSQL database dump complete
+--
+`
+
+	_, err := db.Exec(sq)
+	if err != nil {
+
+	}
+
 }
